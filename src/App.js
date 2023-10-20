6e9e6848e2a88636188de677bdc53093163fc62b
KouMa24 <andresvillagonza24@gmail.com>
2023-10-20 15:55:29 -0600

    Pagina Categorie1 realizada

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Categorie1 from "./pages/Categorie1"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={< Home />} />
+		  <Route path="/categorie1" element={< Categorie1 />} />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/Categorie1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Categorie1 = () => {
+  return (
+    <div>Categorie1</div>
+  )
+}
+
+export default Categorie1
