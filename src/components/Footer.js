050c402ac222031fe63707544310e157554e310e
DanielFCH <danielflrs.ch@gmail.com>
2023-10-25 10:41:32 -0600

    futer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,50 +7,18 @@ const Footer = ({ id }) => {
 		<div className="footer" id={id}>
 			<div className='sb__footer section__padding'>
 				<div className='sb__footer-links'>
-					<div className='sb__footer-links-div'>
-						<h4>Secciones</h4>
-						<a href='/'>
-							<p>Employer</p>
-						</a>
-						<a href='/'>
-							<p>Employer</p>
-						</a>
-						<a href='/'>
-							<p>Employer</p>
-						</a>
-					</div>
-					<div className='sb__footer-links-div'>
-						<h4>Contacto</h4>
-						<a href='#'>
-							<p>Employer</p>
-						</a>
-						<a href='/'>
-							<p>Employer</p>
-						</a>
-						<a href='/'>
-							<p>Employer</p>
-						</a>
-					</div>
-					<div className='sb__footer-links-div'>
-						<h4>Soporte</h4>
-						<a href='#'>
-							<p>Employer</p>
-						</a>
-						<a href='/'>
-							<p>Employer</p>
-						</a>
-						<a href='/'>
-							<p>Employer</p>
-						</a>
-					</div>
-					<div className='sb__footer-links-div'>
+				<div className='sb__footer-links-div'>
 						<h4>Redes Sociales</h4>
 						<div className='social'>
-							<a href='https://www.w3schools.com/'><FaInstagram className='iconee instagram'/></a>
-							<a href='https://www.w3schools.com/'><FaFacebook className='iconee facebook'/></a>
-							<a href='https://www.w3schools.com/'><FaLinkedin className='iconee linkedin'/></a>
+							<a href='https://instagram.com/parque_lalibertad?igshid=MzRlODBiNWFlZA=='><FaInstagram className='iconee instagram'/></a>
+							<a href='https://www.facebook.com/cetavcr?mibextid=ZbWKwL'><FaFacebook className='iconee facebook'/></a>
+							<a href='https://www.linkedin.com/in/cetav-parque-la-libertad-832002249/?originalSubdomain=cr'><FaLinkedin className='iconee linkedin'/></a>
 						</div>
 					</div>
+					<div className='sb__footer-links-div'>
+						<h4 className='secciones'>Secciones</h4>
+					</div>
+					
 				</div>
 				<hr></hr>
 				<div className='sb__footer-below'>
