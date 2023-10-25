5481f618fccbb2a888f3e72c2a5a2f4c7047b365
KouMa24 <andresvillagonza24@gmail.com>
2023-10-25 09:47:31 -0600

    Intento de boton #1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,21 @@ import Title from '../components/Title';
 import Favorite from '../components/Favorite';
 import Categories from '../components/Categories';
 import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
 
 function App() {
 	return (
-
-		<>
-		<Title></Title>
-		<Categories id="menu"></Categories>
-		<Favorite id="favorite"></Favorite>
-		<Footer id="footer"></Footer>
-		</>
+		<><div>
+			{/* Botón para redirigir a /category1 */}
+			<Link to="/category1">
+				<button>Ir a Category 1</button>
+			</Link>
+		</div><>
+				<Title></Title>
+				<Categories id="menu"></Categories>
+				<Favorite id="favorite"></Favorite>
+				<Footer id="footer"></Footer>
+			</></>
 
 	);
 }
