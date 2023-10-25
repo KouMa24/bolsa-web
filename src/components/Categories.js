7e6aab4aa19e5c5d14abcb77748542530bfe5835
KouMa24 <andresvillagonza24@gmail.com>
2023-10-25 14:18:23 -0600

    again link

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -2,31 +2,35 @@ import React from 'react';
 import './Categories.css';
 import { Categories } from '../data/menu';
 import Slide from 'react-reveal/Reveal';
+import { Link } from 'react-router-dom';
 
-const categories = ({ id }) => {
-	return (
-		<div className="categoriesContainer" id={id}>
-			<h1 className="categoriesHeading">Categorías</h1>
-			<div className="categoriesWrapper">
-				{Categories.map((Categories, index) => (
-					<Slide
-						key={index}
-						left={index >= 5 ? true : false}
-						right={index <5 ? true : false}
-					>
-						<div className="categoriesCard">
-							<img src={Categories.img} className="categoriesImg" alt={Categories.alt} />
-							<div className="categoriesInfo">
-								<div className="protTitle">{Categories.title}</div>
-								<div className="protDesc">{Categories.desc}</div>
-								<button className="categoriesBtn">Ver más</button>
-							</div>
-						</div>
-					</Slide>
-				))}
-			</div>
-		</div>
-	);
+const CategoriesComponent = ({ id }) => {
+  return (
+    <div className="categoriesContainer" id={id}>
+      <h1 className="categoriesHeading">Categorías</h1>
+      <div className="categoriesWrapper">
+        {Categories.map((category, index) => (
+          <Slide
+            key={index}
+            right={index >= 4 ? true : false}
+            left={index < 4 ? true : false}
+          >
+            <div className="categoriesCard">
+              <img src={category.img} className="categoriesImg" alt={category.alt} />
+              <div className="categoriesInfo">
+                <div className="protTitle">{category.title}</div>
+                <div className="protDesc">{category.desc}</div>
+                <Link to={`/category${index + 1}`}>
+                  <button className="categoriesBtn">Ver más</button>
+                </Link>
+              </div>
+            </div>
+          </Slide>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default categories;
+export default CategoriesComponent;
+
