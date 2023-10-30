8ca6956742a229cc5210245a497f1a9fbb737d6c
SantyDN2710 <gnavarretevega@gmail.com>
2023-10-30 10:31:04 -0600

    categorys

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Category1 from "./pages/Category1"
+import Category4 from "./pages/Category4";
+import Category5 from "./pages/Category5";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={< Home />} />
 		      <Route path="/category1" element={< Category1 />} />
+          <Route path="/category4" element={< Category4 />} />
+          <Route path="/category5" element={< Category5 />} />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/Category5.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar22 from "../components/Navbar22";
+import Ofertas from "../components/Ofertas";
+
+const Category5 = () => {
+  return (
+
+      <><Navbar22></Navbar22>
+      <Ofertas></Ofertas></>
+  
+  )
+}
+
+export default Category5
