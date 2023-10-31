55c7a07b7d8c187e26bb12282ac40c0f1ffdf133
DanielFCH <danielflrs.ch@gmail.com>
2023-10-30 19:33:21 -0600

    categorias

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Category1 from "./pages/Category1"
+import Category1 from "./pages/Category1";
+import Category2 from "./pages/Category2";
+import Category3 from "./pages/Category3";
 import Category4 from "./pages/Category4";
 import Category5 from "./pages/Category5";
 
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={< Home />} />
 		      <Route path="/category1" element={< Category1 />} />
+          <Route path="/category2" element={< Category2 />} />
+          <Route path="/category3" element={< Category3 />} />
           <Route path="/category4" element={< Category4 />} />
           <Route path="/category5" element={< Category5 />} />
         </Routes>
