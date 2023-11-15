7fda41daaa5a4e70b2f52f678d1bff683b88a1ae
Isaac04GJ <isaacgj0408@gmail.com>
2023-11-15 07:43:16 -0600

    imagenes de la base de datos

--- src/components/DisplayCompany.js
+++ src/components/DisplayCompany.js
@@ -6,7 +6,8 @@ function CompanyTable({company}) {
       <div>{company.fields.date}</div>
       <div>{company.fields.name_company}</div>
       <div>{company.fields.description}</div>
-	  <div>{company.fields.career}</div>
+	    <div>{company.fields.career}</div>
+      <div>{company.fields.logo && <img className="card-img-left" src={company.fields.logo[0].url} />}</div>
       <br/>
     </div>
    );
